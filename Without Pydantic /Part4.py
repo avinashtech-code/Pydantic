30932062d1c0a4cf17dfb89a7477985aa0239228
avinashtech-code <avinash.tech149@gmail.com>
2026-07-12 03:59:21 +0530

    Create Part4.py

---
+++ Without Pydantic /Part4.py
@@ -0,0 +1,13 @@
+def insert_patient_data(name: str, age: int):
+
+    if type(name) == str and type(age) == int:
+        if age < 0:
+            raise ValueError('Age cant be negative')
+        else:
+            print(name)
+            print(age)
+            print('inserted into database')
+    else:
+        raise TypeError('Incorrect data type')
+      
+insert_patient_data('nitish', '30')
